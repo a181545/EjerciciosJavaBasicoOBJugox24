127c8911d39028457a3194a888f7bc2a4d730596
a181545 <114413070+a181545@users.noreply.github.com>
2022-12-11 12:49:38 -0600

    Add files via upload

---
+++ Ej3JBJugox24.java
@@ -0,0 +1,9 @@
+public class Ej3JBJugox24 {
+    public static void main(String[] args) {
+        String[] nombres = {"Hugo", "Mauricio", "Jorge", "Emilio", "Héctor", "Edgar", "Ramón", "José"};
+
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+        }
+    }
+}
